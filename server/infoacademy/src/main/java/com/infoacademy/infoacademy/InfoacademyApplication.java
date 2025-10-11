c5eff5ff943689020093203ee1f321aac3435022
Ahmed <ahmed.benhajamorr@gmail.com>
2025-10-11 14:55:24 +0100

    first commit

---
+++ server/infoacademy/src/main/java/com/infoacademy/infoacademy/InfoacademyApplication.java
@@ -0,0 +1,13 @@
+package com.infoacademy.infoacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoacademyApplication.class, args);
+	}
+
+}
---
+++ server/infoacademy/src/test/java/com/infoacademy/infoacademy/InfoacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoacademy.infoacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
